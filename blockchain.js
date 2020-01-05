e30df91edb27478ce347ec5244fbeb4774de6996
yeshi wangdi <wangdzi@skywalker.fios-router.home>
2020-01-05 13:05:52 -0500

    installed js-sha256 n blockchain can now generate hash

--- blockchain.js
+++ blockchain.js
@@ -1,3 +1,5 @@
+let sha256 = require('./js-sha256')
+
 /**
  * genesisBlock: bascically contains some kind of configurational stuff. for example in bitcoin, the genesisBlock is the first block at index 0 and that wil contain different kind of rewards for the miners
  * genesisBlock is a required block and doesnt have any previous hash
@@ -5,6 +7,21 @@
 
 class Blockchain {
   constructor(genesisBlock) {
-      this.block = []
+      this.blocks = []
+      this.addBlock(genesisBlock)
+  }
+
+  addBlock(block) {
+    if(this.blocks.length === 0) {
+      block.previousHash = "00000000000"
+      block.hash = this.generateHash(block)
+    }
+
+    this.blocks.push(block)
+  }
+
+  generateHash(block) {
+    let hash = sha256(block.key)
+    return hash
   }
 }
