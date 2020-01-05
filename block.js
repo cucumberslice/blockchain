affc21b48d7e74ba36513d3d8ba592bfc10e38e0
yeshi wangdi <wangdzi@skywalker.fios-router.home>
2020-01-05 12:58:42 -0500

    added get key fn and addTransaction fn

--- block.js
+++ block.js
@@ -1,10 +1,20 @@
 class Block {
   constructor() {
     this.index = 0
-    this.previousHas = ''
+    this.previousHash = ''
     this.hash = ''
     this.nonce = 0
-    this.transaction = []
+    this.transactions = []
+  }
+
+  // key will generate the hash function and that hash function is going to return the sha-256 hash
+  get key() {
+    return JSON.stringify(this.transaction + this.index + this.previousHash + this.nonce)
+  }
+
+  //add transactions to the instance of the block
+  addTransaction(transaction) {
+    this.transactions.push(transaction)
   }
 }
 
