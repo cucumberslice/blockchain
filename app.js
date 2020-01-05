1f375fe71fb1b88ac8341767af08baed9c2e612b
yeshi wangdi <wangdzi@skywalker.fios-router.home>
2020-01-05 13:42:32 -0500

    app.js can now generate blocks

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ let transaction = new Transaction('Mary', 'jerry', 100)
 let genesisBlock = new Block()
 let blockchain = new Blockchain(genesisBlock)
 
+let block = blockchain.getNextBlock([transaction])
+blockchain.addBlock(block)
+
+let anotherTransaction = new Transaction('Azam', 'Jerry', 10)
+let block1 = blockchain.getNextBlock([anotherTransaction,transaction])
+blockchain.addBlock(block1)
+
 console.log(blockchain)
--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 let sha256 = require('js-sha256')
+let Block = require('./block')
 
 /**
  * genesisBlock: bascically contains some kind of configurational stuff. for example in bitcoin, the genesisBlock is the first block at index 0 and that wil contain different kind of rewards for the miners
@@ -20,8 +21,32 @@ class Blockchain {
     this.blocks.push(block)
   }
 
+  getNextBlock(transactions) {
+    let block = new Block()
+
+    transactions.forEach(transaction => {
+      block.addTransaction(transaction)
+    });
+
+    let previousBlock = this.getPreviousBlock()
+    block.index = this.blocks.length
+    block.previousHash = previousBlock.hash
+    block.hash  = this.generateHash(block)
+
+    return block
+  }
+
+  getPreviousBlock() {
+    return this.blocks[this.blocks.length - 1]
+  }
+
   generateHash(block) {
     let hash = sha256(block.key)
+    while(!hash.startsWith('000')) {
+      block.nonce += 1;
+      hash = sha256(block.key)
+      console.log(hash)
+    }
     return hash
   }
 }
