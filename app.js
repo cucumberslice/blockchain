04b37f90abbc79a0164abeb6d9b6da8215c5ebf5
yeshi wangdi <wangdzi@skywalker.fios-router.home>
2020-01-07 11:13:00 -0500

    express routes can now post new transactons

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app  = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let Block = require('./block')
 let Blockchain = require('./blockchain')
 let Transaction = require('./transaction')
 
+let transactions = []
+
 app.get('/', function(req,res) {
   res.send('hello world')
+})
+
+app.post('/transactions', function(req,res) {
+
+  let to = req.body.to
+  let from = req.body.from
+  let amount = req.body.amount
+
+  let transaction = new Transaction(from, to, amount)
+
+  transactions.push(transaction)
+
+  res.json(transactions)
 })
 
 app.get('/blockchain', function(req,res) {
